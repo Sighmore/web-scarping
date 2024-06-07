76fa0e769fa8667cf95bb28e032397c35161b38c
INOXOFT <simonkuriah7@gmail.com>
2024-06-07 11:02:38 +0300

    dynamic market data

--- scraperapi2.js
+++ scraperapi2.js
@@ -1,24 +1,32 @@
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 let scraped_headlines = [];
+
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     try {
-        await page.goto('https://www.reddit.com/r/webscraping/', {timeout: 180000});
-        let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+        await page.goto('https://www.investing.com/currencies/eur-usd-chart', {timeout: 180000});
+        let bodyHTML = await page.evaluate(() => {
+            const div = document.createElement('div');
+            div.className = 'text-5xl/9 font-bold text-[#232526] md:text-[42px] md:leading-[60px]';
+            div.setAttribute('data-test', 'instrument-price-last');
+            div.textContent = '';
+            document.body.appendChild(div);
+            return document.body.innerHTML;
+        });
         let $ = cheerio.load(bodyHTML);
-        let article_headlines = $('a[href*="/r/webscraping/comments"] > div')
+        let article_headlines = $('a[href*="/r/webscraping/comments"] > div');
         article_headlines.each((index, element) => {
-        title = $(element).find('h3').text()
-        scraped_headlines.push({
-        'title': title
-        })
-});
-}
-catch(err) {
-console.log(err);
-}
-await browser.close();
-console.log(scraped_headlines)
+            title = $(element).find('h3').text();
+            scraped_headlines.push({
+                'title': title
+            });
+        });
+    }
+    catch(err) {
+        console.log(err);
+    }
+    await browser.close();
+    console.log(scraped_headlines);
 })();
