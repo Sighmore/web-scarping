feeb521a63c0bd13121bd359947413fb2cc424a4
Simon Githongo Kuria <105274094+Sighmore@users.noreply.github.com>
2024-06-10 07:05:17 +0300

    comments

--- scraperapi.js
+++ scraperapi.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 const cheerio = require('cheerio')
-const url = 'https://github.com/Sighmore';
+const url = 'https://github.com/Sighmore';//reades the repositorieson github
     axios(url)
         .then(response => {
             const html = response.data;
             const $ = cheerio.load(html)
-            const repositories = $('.pinned-item-desc').text()
+            const repositories = $('.pinned-item-desc').text()//classname of the pinnnd github repos
             console.log(repositories);
         })
         .catch(console.error);
